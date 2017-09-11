097cf4f3eb47d943e79a40cc983f94065cb25ddc
Augusto Rende <augustorendegiacomelli@gmail.com>
2017-09-10 21:59:22 -0300

    Mejoras estéticas de toda la WebApp

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -5,30 +5,33 @@
 		$cant_page = floor($items->total()/$items->perPage())+1; 
 	}
 ?>
-<nav aria-label="pagination">
-  	<ul class="pagination pagination-sm justify-content-center">
-  		@if($items->currentPage() == 1)
-		    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">First</a></li>
-		    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Previous</a></li>
-	    @else
-		    	<li class="page-item"><a class="page-link" href="{{ route($controller.'.index').'?page=1' }}">First</a></li>
-		    	<li class="page-item"><a class="page-link" href="{{ $items->previousPageUrl() }}">Previous</a></li>
-	    @endif
+<?php if ($cant_page > 1) { ?>
+	<nav aria-label="pagination">
+	  	<ul class="pagination pagination-sm justify-content-center">
+	  		@if($items->currentPage() == 1)
+			    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">First</a></li>
+			    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Previous</a></li>
+		    @else
+			    	<li class="page-item"><a class="page-link" href="{{ route($controller.'.index').'?page=1' }}">First</a></li>
+			    	<li class="page-item"><a class="page-link" href="{{ $items->previousPageUrl() }}">Previous</a></li>
+		    @endif
 
-	    @for ($i=1; $i<($cant_page+1); $i++)
-		    @if ($items->currentPage() == $i)
-	    		<li class="page-item disabled"><a class="page-link" href="#">{{$i}}</a></li>
-    		@else
-	    		<li class="page-item"><a class="page-link" href="{{ route($controller.'.index').'?page='.$i }}">{{$i}}</a></li>
-    		@endif
-		@endfor
+		    @for ($i=1; $i<($cant_page+1); $i++)
+			    @if ($items->currentPage() == $i)
+		    		<li class="page-item disabled"><a class="page-link" href="#">{{$i}}</a></li>
+	    		@else
+		    		<li class="page-item"><a class="page-link" href="{{ route($controller.'.index').'?page='.$i }}">{{$i}}</a></li>
+	    		@endif
+			@endfor
 
-  		@if($items->currentPage() == $cant_page)
-		    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Next</a></li>
-		    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Last</a></li>
-	    @else
-    		<li class="page-item"><a class="page-link" href="{{ $items->nextPageUrl() }}">Next</a></li>
-    		<li class="page-item"><a class="page-link" href="{{ route($controller.'.index').'?page='.$cant_page }}">Last</a></li>
-	    @endif
-  </ul>
-</nav>
+	  		@if($items->currentPage() == $cant_page)
+			    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Next</a></li>
+			    	<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Last</a></li>
+		    @else
+	    		<li class="page-item"><a class="page-link" href="{{ $items->nextPageUrl() }}">Next</a></li>
+	    		<li class="page-item"><a class="page-link" href="{{ route($controller.'.index').'?page='.$cant_page }}">Last</a></li>
+		    @endif
+	    </ul>
+	</nav>
+<?php } ?>
+
